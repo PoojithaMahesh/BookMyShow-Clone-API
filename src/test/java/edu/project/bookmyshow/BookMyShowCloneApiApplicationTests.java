1001f4d9f5ecbd66171a69b0e12d5ca648bd0839
rajumb0232 <rajugowda0232@gmail.com>
2023-04-26 10:43:35 +0530

    created Project - BookMyShow-Clone-API

---
+++ src/test/java/edu/project/bookmyshow/BookMyShowCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.project.bookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
